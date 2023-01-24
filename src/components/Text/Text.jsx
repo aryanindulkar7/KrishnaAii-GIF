c360da594804572e06a72f32879ef113364bf8d3
aryanindulkar7 <aryanindulkar7@gmail.com>
2023-01-24 15:16:38 +0530

    first commit

---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,22 @@
+import "./Text.css";
+function Text(){
+    return(
+        <div className="wrapper">
+        <h3 className="fade">
+            <span>K</span>
+            <span>r</span>
+            <span>i</span>
+            <span>s</span>
+            <span>h</span>
+            <span>n</span>
+            <span>a</span>
+            <span>A</span>
+            <span>i</span>
+            <span>i</span>
+
+        </h3>
+    </div>
+    )
+}
+
+export default Text;
---
+++ src/components/feather/Leaf.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Lottie from "lottie-react";
+import z1lvuUXCKc from "./z1IvuUXCKc.json";
+
+const Leaf = () =>
+
+<div style={{ width:"30%"}}>
+
+<Lottie animationData={z1lvuUXCKc} loop={true} />
+
+</div>
+
+export default Leaf;
